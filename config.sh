3f4f8efa40b1c9e017791104638dfaa8fd048af0
Priya Gupta <priyagupta14317@gmail.com>
2019-07-09 13:20:53 -0500

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,14 @@
+Host anserver
+  User ubuntu
+  Hostname -----
+  Identityfile ~/--.pem
+
+Host webserver
+  User ubuntu
+  Hostname -----
+  Identityfile ~/----.pem
+
+Host appserver
+  User ubuntu
+  Hostname ---
+  Identityfile ~/-----.pem
